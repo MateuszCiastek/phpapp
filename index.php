6b23b689239747d37a33628b6d8c49bec0f8a785
MateuszCiastek <mateusz.pyrka@gmail.com>
2019-12-18 14:40:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ input[type=submit] {width: 8em;  height: 2em;}
 <?php
 /* index.php */
 require_once('soap/lib/nusoap.php');
-$wsdl = 'http://localhost/soap/index.php?wsdl';
+$wsdl = 'http://phpapp-homesite.192.168.0.130.nip.io/soap/index.php?wsdl';
 $client = new nusoap_client($wsdl, 'wsdl');
 $params = array('time_format' => 'Y-m-d D H:i:s T');
 $response1 = $client->call('getTime', $params);
